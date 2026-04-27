0964fd9e1a00c4203afab3612b43712b2ea31ec0
patttterson <49377371+patttterson@users.noreply.github.com>
2026-04-27 01:56:07 -0700

    feat: short git commit hash

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,11 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { execSync } from 'child_process';
 
-export default defineConfig({ plugins: [tailwindcss(), sveltekit()] });
+const commitHash = execSync('git rev-parse --short HEAD').toString().trim();
+
+export default defineConfig({
+	plugins: [tailwindcss(), sveltekit()],
+	define: { __COMMIT_HASH__: JSON.stringify(commitHash) },
+});
